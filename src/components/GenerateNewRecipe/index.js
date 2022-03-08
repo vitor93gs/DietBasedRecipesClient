d97d9d67d6b46fa88dc984c76de5a3c09784a245
Vitor Santos <vitor93gs@gmail.com>
2022-03-08 17:12:33 -0300

    mais trabalho no post das recipes geradas pela ia

--- src/components/GenerateNewRecipe/index.js
+++ src/components/GenerateNewRecipe/index.js
@@ -13,14 +13,27 @@ export function GenerateNewRecipe(){
     const [response, setResponse] = useState("")
     
     
-    const configuration = new Configuration({ apiKey :"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"})
+    const configuration = new Configuration({ apiKey :process.env.REACT_APP_OPENAIKEI})
     
     const openai = new OpenAIApi(configuration);
 
+    useEffect(() => {
+        if(response!==""){
+            async function handlePost(){
+                console.log(response)
+                const postedRecipe = await api.post("/recipes/newRecipe", response)
+                console.log(postedRecipe)
+                
+            }
+            handlePost()
+        }
+    },[response])
+
     function handleChange(event){
         setForm({...form, [event.target.name] : event.target.value})
-        setPrompt(`Generate a JSON containing a diet recipe with a name, ingredients and instructions, using the following ingredients: ${form.firstIngredient} ${form.secondIngredient} ${form.thirdIngredient} ${form.fourthIngredient} ${form.fifthIngredient}`)
+        setPrompt(`Generate a JSON containing a diet recipe with a name, ingredients and instructions (instructions must be a single string without line breaks), using the following ingredients: ${form.firstIngredient} ${form.secondIngredient} ${form.thirdIngredient} ${form.fourthIngredient} ${form.fifthIngredient}`)
     }
+   
 
     function handleSubmit(e){
         e.preventDefault()
@@ -34,7 +47,6 @@ export function GenerateNewRecipe(){
         }).then((res) => {
             setResponse(res.data.choices[0].text)
         })
-        
     }
     
     return <>
@@ -50,7 +62,7 @@ export function GenerateNewRecipe(){
             <FormField type="text" label="5" name="fifthIngredient" id="FifthIngredient" readOnly={false} required={false} value={form.fifthIngredient} onChange={handleChange} pattern="^[A-Za-z ]{1,30}$"/>
             <br/>
             <button type="submit" > Generate! </button>
-        </form>
+        </form>       
         
     </>
 }
