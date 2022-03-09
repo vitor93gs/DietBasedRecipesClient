209280229f320bc07237eb25539a427ad7127a08
Vitor Santos <vitor93gs@gmail.com>
2022-03-09 15:30:44 -0300

    feita lógica da pagina principal, ajustada a api da ia para retornar sempre valores parecidos, criada lógica para salvar recipes como favoritos

--- src/components/GenerateNewRecipe/index.js
+++ src/components/GenerateNewRecipe/index.js
@@ -1,7 +1,9 @@
 import { FormField } from "../../components/FormField"
+import { useContext } from "react"
 import { useEffect, useState } from "react"
-// import { PresentRecipe } from "../PresentRecipe"
+import { PresentRecipe } from "../PresentRecipe"
 import { api } from "../../api"
+import { AuthContext } from "../../contexts/authContext"
 
 const { Configuration, OpenAIApi } = require("openai")
 
@@ -11,8 +13,10 @@ export function GenerateNewRecipe(){
     const [form, setForm] = useState({firstIngredient: "", secondIngredient: "", thirdIngredient: "", fourthIngredient: "", fifthIngredient: ""})
     const [prompt, setPrompt] = useState("")
     const [response, setResponse] = useState("")
-    
-    
+    const [recipeObject , setRecipeObject] = useState({})
+    const { loggedInUser } = useContext(AuthContext);
+
+
     const configuration = new Configuration({ apiKey :process.env.REACT_APP_OPENAIKEI})
     
     const openai = new OpenAIApi(configuration);
@@ -20,10 +24,10 @@ export function GenerateNewRecipe(){
     useEffect(() => {
         if(response!==""){
             async function handlePost(){
-                console.log(response)
-                const postedRecipe = await api.post("/recipes/newRecipe", response)
+                const obj = (JSON.parse(response))
+                const postedRecipe = await api.post("/recipes/newRecipe", obj)
                 console.log(postedRecipe)
-                
+                setRecipeObject(postedRecipe.data)
             }
             handlePost()
         }
@@ -31,7 +35,16 @@ export function GenerateNewRecipe(){
 
     function handleChange(event){
         setForm({...form, [event.target.name] : event.target.value})
-        setPrompt(`Generate a JSON containing a diet recipe with a name, ingredients and instructions (instructions must be a single string without line breaks), using the following ingredients: ${form.firstIngredient} ${form.secondIngredient} ${form.thirdIngredient} ${form.fourthIngredient} ${form.fifthIngredient}`)
+        setPrompt(`Using this JSON as a template: {"name":,"ingredients":,"instructions":}, populate the JSON with a diet recipe with a custom name, ingredients (ingredients must be separated in strings inside an array) and instructions using at least one of the given ingredients.
+
+        ingredients: chicken bread crumbs parsley
+        JSON: {"name":"Chicken Parmesan","ingredients":["chicken breast","bread crumbs","grated cheese","parsley","garlic","salt","pepper","eggs","milk","olive oil"],"instructions":"Preheat the oven to 200 degrees C. Cut the chicken breast into thin slices. In a bowl, mix together the bread crumbs, grated cheese, parsley, garlic, salt, pepper, eggs and milk. Dip each chicken slice into the mixture, then place on a lightly oiled baking sheet. Bake for 20 minutes, or until cooked through."}
+        ingredients: pork belly garlic
+        JSON:{"name":"Garlic Pork Belly","ingredients":["1 pound pork belly","1 head garlic","1 tablespoon olive oil","1 teaspoon salt","1/2 teaspoon black pepper"],"instructions":" Preheat oven to 375 degrees. Peel garlic cloves and slice in half. In a large oven-proof skillet, heat olive oil over medium-high heat. Add pork belly and garlic cloves. Cook for 3-5 minutes per side, or until pork is browned. Sprinkle with salt and pepper. Transfer skillet to oven and bake for 30-40 minutes, or until pork is cooked through. Serve hot."}
+        ingredients: brocoli cheese parsley
+        JSON: {"name":"Broccoli Parsley Cheese Soup","ingredients":["1 cup chopped onion","1/4 cup chopped celery","1/4 cup chopped green bell pepper","1/4 cup chopped red bell pepper","1/4 cup chopped yellow bell pepper","1/4 cup chopped parsley","1/4 cup chopped broccoli","1/4 cup shredded cheddar cheese","1/4 cup heavy cream","1/4 teaspoon black pepper","1/4 teaspoon garlic powder","1/4 teaspoon onion powder","1/4 teaspoon celery seed","1/4 teaspoon dried thyme","1/4 teaspoon dried basil","1/4 teaspoon dried oregano","1/4 teaspoon salt","1/4 teaspoon sugar","1/4 cup all-purpose flour","4 cups chicken broth","1 cup water"],"instructions":"In a large saucepan, sauté onion, celery, bell peppers, parsley, broccoli, and cheese in butter until vegetables are tender. Add cream, black pepper, garlic powder, onion powder, celery seed, thyme, basil, oregano, salt, and sugar; stir well. Sprinkle with flour; stir until well blended. Add chicken broth and water; simmer for 20 minutes. Serve in individual bowls."}
+        ingredients: ${form.firstIngredient} ${form.secondIngredient} ${form.thirdIngredient} ${form.fourthIngredient} ${form.fifthIngredient}.
+        JSON:`) 
     }
    
 
@@ -48,7 +61,18 @@ export function GenerateNewRecipe(){
             setResponse(res.data.choices[0].text)
         })
     }
-    
+    function handleClick(){
+        PresentRecipe(recipeObject.name, recipeObject.ingredients, recipeObject.instructions)
+    }
+    async function handleFavorite(){
+        try {
+            const postedRecipe = await api.patch(`/recipes/makeFavorite/${recipeObject._id}`)
+            console.log(postedRecipe)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return <>
         <h1>
         CREATE YOUR OWN DIET RECIPE
@@ -62,7 +86,8 @@ export function GenerateNewRecipe(){
             <FormField type="text" label="5" name="fifthIngredient" id="FifthIngredient" readOnly={false} required={false} value={form.fifthIngredient} onChange={handleChange} pattern="^[A-Za-z ]{1,30}$"/>
             <br/>
             <button type="submit" > Generate! </button>
-        </form>       
+        </form> 
+        {recipeObject._id ? <h1>{recipeObject.name} <button onClick={handleClick}>Show Recipe</button>{loggedInUser.token ? <button onClick={handleFavorite}> Adicionar aos favoritos</button>:"" }</h1> : ""}      
         
     </>
 }
